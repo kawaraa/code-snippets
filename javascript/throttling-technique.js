6747ece9b7201938753d81d181e391d55f6ec7a0
Kawara <39919017+kawaraa@users.noreply.github.com>
2025-05-15 04:47:40 +0200

    Improving.

--- javascript/throttling-technique.js
+++ javascript/throttling-technique.js
@@ -8,50 +8,17 @@ const rateLimit = require("express-rate-limit");
 
 const app = express();
 
-// ==== Solution-1 =====
-const requestCounts = new Map();
+// ==== Solution-1 Using "k-utility" =====
+const rateLimiter = require("k-utilities/network.js");
+app.use(rateLimiter); // Apply the rate limiting middleware to all routes
 
-// Define the rate limit parameters
-const WINDOW_SIZE = 60 * 1000; // 1 minute
-const MAX_REQUESTS = 40;
-
-// Middleware function to implement rate limiting
-function rateLimit(req, res, next) {
-  const clientIp = req.ip; // Get client's IP address
-  const newRequestData = { count: 1, timestamp: Date.now() };
-  const requestData = requestCounts.get(clientIp);
-
-  if (!requestData) requestCounts.set(clientIp, newRequestData);
-  else {
-    const exceededTheMaximum = newRequestData.timestamp - requestData.timestamp < WINDOW_SIZE;
-    requestData.count++;
-
-    if (requestData.count > MAX_REQUESTS && exceededTheMaximum) {
-      // If the request count exceeds the maximum, send a 429 response
-      return res.status(429).send("Too many requests, please try again later.");
-    } else {
-      // If the time window has elapsed, reset the request count
-      requestCounts.set(clientIp, requestData);
-    }
-  }
-
-  // Continue to the next middleware or route handler
-  next();
-}
-
-// Apply the rate limiting middleware to all routes
-app.use(rateLimit);
-
-// ==== Solution-2 =====
-// // Using "express-rate-limit" => Define the rate limit options
-// const limiter = rateLimit({
-//   windowMs: 15 * 60 * 1000, // 15 minutes
-//   max: 100, // limit each IP to 100 requests per windowMs
-//   message: "Too many requests, please try again later.",
-// });
-
-// // Apply the rate limiter to all requests
-// app.use(limiter);
+// ==== Solution-2 Using "express-rate-limit" =====
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+  message: "Too many requests, please try again later.",
+});
+app.use(limiter); //  Apply the rate limiter to all requests
 
 app.get("/", (req, res) => res.json("Request processed successfully"));
 
